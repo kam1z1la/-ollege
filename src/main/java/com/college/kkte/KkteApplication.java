1718f1a5a4e63194fa6b30152e0178c460150369
vadim novitskyi <thrownovitskyi@gmail.com>
2023-09-20 13:38:07 +0300

    create architecture

---
+++ src/main/java/com/college/kkte/KkteApplication.java
@@ -0,0 +1,13 @@
+package com.college.kkte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/college/kkte/news/NewsService.java
@@ -0,0 +1,4 @@
+package com.college.kkte.news;
+
+public class NewsService {
+}
